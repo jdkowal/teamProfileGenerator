ea05accbf45447dc97938e849ea65fd1d9d99434
Joe Kowalczyk <josephdkowalczyk61@gmail.com>
2021-10-25 20:39:11 -0500

    need to generate markup

--- index.js
+++ index.js
@@ -3,124 +3,251 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require("./src/markdown");
+const renderManager = require("./src/markdown");
+const { validate } = require("@babel/types");
+const { number } = require("yargs");
+const { not } = require("expect");
 
-const employeeArray = ["Engineer","Intern","I do no want to add any more team members."]
+const employeeArray = ["Engineer", "Intern", "I do no want to add any more team members."]
+const teamArray = []
 
-const managerQuestions = [
-    {
-        type:"input",
-        name: "Name",
-        message: "What is the team member's name?"
-    },
-    {
-        type:"input",
-        name: "idNum",
-        message: "What is the team member's ID number?"
-    },
-    {
-        type:"input",
-        name: "email",
-        message: "What is the member's email?"
-    },
-    {
-        type:"input",
-        name: "officeNumber",
-        message: "What is the team manager's office number?"
+
+function appMenu() {
+function createManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is the team member's name?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "please enter at least one character"
+            }
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is the team member's ID number?",
+            validate: answer => {
+                const pass = answer.match(
+                    /^[1-9]\d*$/
+                );
+                if (pass) {
+                    return true;
+                }
+                return "please enter a positive number greater tha zero"
+            }
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is the member's email?",
+            validate: answer => {
+                const pass = answer.match(
+                    /\S+@\S+\.\S+/
+                );
+                if (pass) {
+                    return true;
+                }
+                return "please enter a valid email address"
+            }
+        },
+        {
+            type: "input",
+            name: "managerOffice",
+            message: "What is the team manager's office number?",
+            validate: answer => {
+                const pass = answer.match(
+                    /^[1-9]\d*$/
+                );
+                if (pass) {
+                    return true;
+                }
+                return "please enter a positive number greater tha zero"
+            }
+        }
+
+    ]).then(answers => {
+        const manager = new Manager(
+            answers.managerName,
+            answers.mangerId,
+            answers.mangerEmail,
+            answers.mangerOffice);
+        teamArray.push(manager);
+        //empty id array for manager id 
+        createTeam();
+    });
+
+}
+
+function newEmployee() {
+    inquirer.prompt([{
+        type: "list",
+        name: "role",
+        message: "Which type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I do no want to add any more team members."]
     }
+    ]).then(userChoice => {
+        switch (userChoice.role) {
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Intern":
+                addIntern();
+                break;
+            default:
+                buildTeam();
+        }
+    });
 
-];
+}
 
-const engineerQuestions = [
-    {
-        type:"input",
-        name: "Name",
-        message: "What is the engineer's name?"
-    },
-    {
-        type:"input",
-        name: "idNum",
-        message: "What is the engineer's ID number?"
-    },
-    {
-        type:"input",
-        name: "email",
-        message: "What is the engineer's email?"
-    },
-    {
-        type:"input",
-        name: "github",
-        message: "What is the engineer's Github profile?"
-    }
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's name?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "please enter at least one character"
+            }
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What is the engineer's ID number?",
+            validate: answer => {
+                const pass = answer.match(
+                    /^[1-9]\d*$/
+                );
+                if (pass) {
+                    return true;
+                }
+                return "please enter a positive number greater tha zero"
+            }
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the engineer's email?",
+            validate: answer => {
+                const pass = answer.match(
+                    /\S+@\S+\.\S+/
+                );
+                if (pass) {
+                    return true;
+                }
+                return "please enter a valid email address"
+            }
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is the engineer's Github profile?",
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "please enter at least one character"}
 
-];
+        }
+    
+    ]).then(answers => {
+        const engineer = new Engineer(
+            answers.engineerName,
+            answers.engineerId,
+            answers.engineerEmail,
+            answers.github);
+        teamArray.push(engineer);
+        //empty id array for manager id 
+        createTeam();
+    })
+}
 
-const internQuestions = [
-    {
-        type:"input",
-        name: "Name",
-        message: "What is the intern's name?"
+function addIntern() {
+    inquirer.prompt([{
+        type: "input",
+        name: "internName",
+        message: "What is the intern's name?",
+        validate: answer => {
+            if (answer !== "") {
+                return true;
+            }
+            return "please enter at least one character"
+        }
+
     },
     {
-        type:"input",
-        name: "idNum",
-        message: "What is the team intern's ID number?"
+        type: "input",
+        name: "internId",
+        message: "What is the team intern's ID number?",
+        validate: answer => {
+            const pass = answer.match(
+                /^[1-9]\d*$/
+            );
+            if (pass) {
+                return true;
+            }
+            return "please enter a positive number greater tha zero"
+        }
     },
     {
-        type:"input",
-        name: "email",
-        message: "What is the intern's email?"
+        type: "input",
+        name: "internEmail",
+        message: "What is the intern's email?",
+        validate: answer => {
+            const pass = answer.match(
+                /\S+@\S+\.\S+/
+            );
+            if (pass) {
+                return true;
+            }
+            return "please enter a valid email address"
+        }
     },
     {
-        type:"input",
+        type: "input",
         name: "school",
-        message: "What school does the intern attend?"
+        message: "What school does the intern attend?",
+        validate: answer => {
+            if (answer !== "") {
+                return true;
+            }
+            return "please enter at least one character"
+        }
+    }]).then(answers => {
+        const intern = new Intern(
+            answers.internName,
+            answers.internId,
+            answers.internEmail,
+            answers.school);
+        teamArray.push(intern);
+        //empty id array for manager id 
+        createTeam();
+    })
+}
+
+
+function buildTeam() {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+
     }
+    fs.writeFileSync(outputPath, render(teamArray), "utf-8");
+}
 
-];
+// console.log("this is my team!");
+createManager();
+newEmployee();
+addEngineer();
+addIntern();
+}
 
-const addNewEmployee =
-{
-    type:"list",
-    name: "license",
-    message: "Which type of team member would you like to add?",
-    choices: employeeArray
-};
-
-function getManagerInfo(){
-    inquirer
-    .prompt(managerQuestions)
-    .then((data)=>{
-        console.log(data)
-        
-})
-
-};
-
-function newEmployee(){
-    inquirer
-    .prompt(addNewEmployee)
-    .then((data)=>{
-        console.log(data)
-        
-})
-};
-
-function getEngineerInfo(){
-    inquirer
-    .prompt(engineerQuestions)
-    .then((data)=>{
-        console.log(data)
-        
-})
-};
-
-function getInternInfo(){
-    inquirer
-    .prompt(internQuestions)
-    .then((data)=>{
-        console.log(data)
-        
-})
-};
-
-getManagerInfo();
+appMenu();
+// console.log(manager.getName());
---
+++ src/markdown.js
@@ -0,0 +1,35 @@
+// const Manager = require("../lib/Manager")
+
+// function generateTeam(team) => {
+//     generateManger(){
+//         return`
+//         <div class="cards-format d-flex justify-content-around flex-wrap">
+//         <div class="card" style="width: 18rem;">
+//             <div class="card-body text-white bg-primary">
+//                 <h5 class="card-title">Project Manager</h5>
+//             </div>
+//             <ul class="list-group list-group-flush">
+//                 <li class="list-group-item">ID:${manager.getName()} </li>
+//                 <li class="list-group-item">Email:${manager.getEmail()} </li>
+//                 <li class="list-group-item">Office Number:${manager.getOfficeNumbe()} </li>
+//             </ul>
+//         </div>
+//     </div>`
+//     };
+// }
+// function renderManager(data){
+//     `<div class="cards-format d-flex justify-content-around flex-wrap">
+//     <div class="cardr" style="width: 18rem;">
+//         <div class="card-body text-white bg-primary">
+//             <h5 class="card-title">Project Manager</h5>
+//         </div>
+//         <ul class="list-group list-group-flush">
+//             <li class="list-group-item">ID:${data.name} </li>
+//             <li class="list-group-item">Email:${data.email} </li>
+//             <li class="list-group-item">Office Number:${data.officeNumber} </li>
+//         </ul>
+//     </div>
+// </div>`
+// }
+
+// module.exports = renderManager
